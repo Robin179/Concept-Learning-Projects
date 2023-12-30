88be28f949073f1d047eb13af583ebfee51ff430
prajjwal <prajjwaltripathi174@gmail.com>
2023-12-30 16:21:28 +0530

    initial commit

---
+++ ReactiveApiApp/src/main/java/com/example/apiwithCompletableFuture/ReactiveApiApp/ReactiveApiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiwithCompletableFuture.ReactiveApiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiAppApplication.class, args);
+	}
+
+}
---
+++ ReactiveApiApp/src/test/java/com/example/apiwithCompletableFuture/ReactiveApiApp/ReactiveApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiwithCompletableFuture.ReactiveApiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
